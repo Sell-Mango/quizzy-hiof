e14508710e970f95ce4cad78de2ead47ec7bad2e
Kjell-Magne <kjell_m.larsen@yahoo.com>
2025-10-17 14:09:11 +0200

    Added: quiz for leksjon 14 og 15 i webapplikasjoner

--- src/data/webapplikasjoner_ulearn/webapp.js
+++ src/data/webapplikasjoner_ulearn/webapp.js
@@ -1,4 +1,5 @@
 import { quiz13 } from "./quiz13-fetch-med-arkitektur";
+import { quiz14 } from "./quiz14-utvide-api-klient";
 
 const quiz_webapp13 = {
   id: 'quiz-webapp-13',
@@ -10,5 +11,25 @@ const quiz_webapp13 = {
   ]
 }
 
+const quiz_webapp14 = {
+  id: 'quiz-webapp-14',
+  title: 'Webapps kapittel 14: Utvide api klient',
+  description:
+    'Test din forståelse av av Webapplikasjoner kapittel 13, utvide API-klient med AbortController, caching og logging.',
+  questions: [
+    ...quiz14
+  ]
+}
 
-export const quizzes = [quiz_webapp13];
+const quiz_webapp15 = {
+  id: 'quiz-webapp-15',
+  title: 'Webapps kapittel 15: REST API-teori og arkitektur-prinsipper',
+  description:
+    'Test din forståelse av av Webapplikasjoner kapittel 15, REST API-teori og arkitektur-prinsipper.',
+  questions: [
+    ...quiz14
+  ]
+}
+
+
+export const quizzes = [quiz_webapp13, quiz_webapp14, quiz_webapp15];
